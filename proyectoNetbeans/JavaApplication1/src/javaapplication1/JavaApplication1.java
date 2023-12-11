357ad273a65b508f18f058413982c1ce4447903a
gabriel <gabrielmedinatorres@hotmail.com>
2023-12-11 09:59:39 +0000

    v3

--- proyectoNetbeans/JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ proyectoNetbeans/JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -3,13 +3,17 @@ package javaapplication1;
 public class JavaApplication1 {
     private int a = 555;
     private int o = 232342;
+    private String nombre;
 
     public static void main(String[] args) {
 
         JavaApplication1 b = new JavaApplication1();
         JavaApplication1 c = new JavaApplication1();
+        c.nombre = "hola";
 
-        System.out.println(b.a + b.o); // TODO code application logic here
+        System.out.println(b.a + b.o);
+        System.out.println(c.nombre);
+
     }
 
 }
