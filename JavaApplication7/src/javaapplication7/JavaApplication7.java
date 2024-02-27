02b039bfa36bb88e0b0df901a75524c9873bba72
gabriel <gabrielmedinatorres@hotmail.com>
2024-02-27 18:46:26 +0000

    v4

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -86,28 +86,42 @@ public class JavaApplication7 {
     }
 
     public static void imprimirSudoku(int[][] board) {
-        for (int fila = 0; fila < board.length; fila++) {
-            for (int columna = 0; columna < board[0].length; columna++) {
-                System.out.print(board[fila][columna] + "|");
+        int size = board.length;
+        int subgridSize = (int) Math.sqrt(size);
 
+        // Imprimir la parte superior del tablero
+        System.out.println("+-------+-------+-------+");
+
+        for (int fila = 0; fila < size; fila++) {
+            System.out.print("| ");
+            for (int columna = 0; columna < size; columna++) {
+                System.out.print(board[fila][columna] + " ");
+                if ((columna + 1) % subgridSize == 0) {
+                    System.out.print("| ");
+                }
             }
             System.out.println();
+
+            // Imprimir las líneas horizontales divisorias
+            if ((fila + 1) % subgridSize == 0) {
+                System.out.println("+-------+-------+-------+");
+            }
         }
     }
 
     public static void main(String[] args) {
         int[][] board = {
-                { 0, 0, 8,  0, 0, 0,  0, 4, 0 },
-                { 0, 6, 0,  0, 0, 3,  0, 0, 0 },
-                { 2, 0, 0,  0, 5, 0,  1, 0, 8 },
+                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
 
-                { 0, 0, 0,  0, 0, 0,  0, 0, 9 },
-                { 8, 0, 0,  0, 1, 0,  7, 0, 5 },
-                { 0, 0, 2,  8, 0, 0,  0, 0, 0 },
+                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
 
-                { 0, 0, 0,  0, 4, 0,  0, 2, 0 },
-                { 0, 0, 3,  0, 0, 5,  4, 0, 7 },
-                { 7, 0, 0,  0, 0, 0,  0, 9, 0 }
+                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
         };
                 /*int[][] boardLimpia = {
                 { 0, 0, 0,  0, 0, 0,  0, 0, 0 },
@@ -122,7 +136,7 @@ public class JavaApplication7 {
                 { 0, 0, 0,  0, 0, 0,  0, 0, 0 },
                 { 0, 0, 0,  0, 0, 0,  0, 0, 0 }
         };*/
-        
+
 
         if (resolverSudoku(board)) {
             System.out.println("Sudoku resuelto:");
